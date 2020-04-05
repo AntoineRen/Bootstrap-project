a3ceeffca1c9d8f08978e671fd0dfa24f7a6630d
Antoine Renou <antoine.renou.pro@gmail.com>
2020-04-05 09:55:52 +0200

    test js

--- js/grid.js
+++ js/grid.js
@@ -8,5 +8,5 @@ function fill(fileUrl){
 
 	fetch(fileUrl).then(f => f.text()).then(t => document.getElementById("code").innerText = t);
 }
-fill('../html_exemples/grid_exemple.txt');
+fill('../html_exemples/grid_exemple.html');
 transfer();
